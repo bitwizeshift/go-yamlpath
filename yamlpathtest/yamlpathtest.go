307bc74fad0f02b257269cbaac7e470632b0cd3b
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2025-04-06 01:03:28 -0400

    Add `yamlpathtest` testing library
    It's frequently useful to have pre-canned `YAMLPath` objects that
    return specific values for testing purposes. To make this possible,
    this leverages the newly added `yamlpath.Option` for custom functions
    to produce `YAMLPath` testing objects that return fixed values.

---
+++ yamlpathtest/yamlpathtest.go
@@ -0,0 +1,141 @@
+/*
+Package yamlpathtest provides test-double utilities to aid in testing code that
+leverages the yamlpath library.
+
+This makes it possible to control what concrete [yamlpath.YAMLPath] instances
+return without requiring specific yaml constructs to do so.
+*/
+package yamlpathtest
+
+import (
+	"gopkg.in/yaml.v3"
+	"rodusek.dev/pkg/yamlpath"
+	"rodusek.dev/pkg/yamlpath/internal/yamlutil"
+)
+
+// Collection returns a [yamlpath.YAMLPath] object that returns the given nodes.
+func Collection(nodes ...*yaml.Node) *yamlpath.YAMLPath {
+	nodes = yamlutil.Normalize(nodes...)
+	fn := func(yamlpath.Collection) yamlpath.Collection {
+		return nodes
+	}
+	return compile(fn)
+}
+
+// String returns a [yamlpath.YAMLPath] object that returns the given string.
+func String(s string) *yamlpath.YAMLPath {
+	node := yamlutil.String(s)
+	fn := func(yamlpath.Collection) *yaml.Node {
+		return node
+	}
+	return compile(fn)
+}
+
+// StringSequence returns a [yamlpath.YAMLPath] object that returns a sequence
+// containing the given strings.
+func StringSequence(strings ...string) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) []string {
+		return strings
+	}
+	return compile(fn)
+}
+
+// Bool returns a [yamlpath.YAMLPath] object that returns the given boolean.
+func Bool(b bool) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) bool {
+		return b
+	}
+	return compile(fn)
+}
+
+// BoolSequence returns a [yamlpath.YAMLPath] object that returns a sequence
+// containing the given booleans.
+func BoolSequence(bools ...bool) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) []bool {
+		return bools
+	}
+	return compile(fn)
+}
+
+// Float returns a [yamlpath.YAMLPath] object that returns the given float.
+func Float(float float64) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) float64 {
+		return float
+	}
+	return compile(fn)
+}
+
+// FloatSequence returns a [yamlpath.YAMLPath] object that returns a sequence
+// containing the given floats.
+func FloatSequence(floats ...float64) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) []float64 {
+		return floats
+	}
+	return compile(fn)
+}
+
+// Int returns a [yamlpath.YAMLPath] object that returns the given integer.
+func Int(i int) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) int {
+		return i
+	}
+	return compile(fn)
+}
+
+// IntSequence returns a [yamlpath.YAMLPath] object that returns a sequence
+// containing the given integers.
+func IntSequence(ints ...int) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) []int {
+		return ints
+	}
+	return compile(fn)
+}
+
+// Object returns a [yamlpath.YAMLPath] object that returns the given value.
+func Object(v any) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) any {
+		return v
+	}
+	return compile(fn)
+}
+
+// ObjectSequence returns a [yamlpath.YAMLPath] object that returns a sequence
+// containing the given values.
+func ObjectSequence(vs ...any) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) []any {
+		return vs
+	}
+	return compile(fn)
+}
+
+// Null returns a [yamlpath.YAMLPath] object that returns a null value.
+func Null() *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) *yaml.Node {
+		return yamlutil.Null()
+	}
+	return compile(fn)
+}
+
+// Error returns a [yamlpath.YAMLPath] object that always returns the given
+// error.
+func Error(err error) *yamlpath.YAMLPath {
+	fn := func(yamlpath.Collection) (yamlpath.Collection, error) {
+		return nil, err
+	}
+	return compile(fn)
+}
+
+var (
+	// True is a YAMLPath that always returns a boolean truthy value.
+	True = Bool(true)
+
+	// False is a YAMLPath that always returns a boolean falsey value.
+	False = Bool(false)
+)
+
+func compile(fn any) *yamlpath.YAMLPath {
+	yp := yamlpath.MustCompile("$.return()",
+		yamlpath.WithFunction("return", fn),
+	)
+	return yp
+}
---
+++ yamlpathtest/yamlpathtest_test.go
@@ -0,0 +1,251 @@
+package yamlpathtest_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"rodusek.dev/pkg/yamlpath"
+	"rodusek.dev/pkg/yamlpath/internal/yamltest"
+	"rodusek.dev/pkg/yamlpath/internal/yamlutil"
+	"rodusek.dev/pkg/yamlpath/yamlpathtest"
+)
+
+func TestCollection(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{
+		yamlutil.String("a"),
+		yamlutil.String("b"),
+		yamlutil.String("c"),
+	}
+	sut := yamlpathtest.Collection(
+		yamltest.MustParseNode(`"a"`),
+		yamltest.MustParseNode(`"b"`),
+		yamltest.MustParseNode(`"c"`),
+	)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`{}`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("Collection(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("Collection(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestString(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{yamlutil.String("test")}
+	sut := yamlpathtest.String("test")
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`"test"`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("String(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("String(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestStringSequence(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{
+		yamltest.MustParseNode(`["a", "b", "c"]`),
+	}
+	sut := yamlpathtest.StringSequence("a", "b", "c")
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`["a", "b", "c"]`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("StringSequence(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("StringSequence(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestBool(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{yamltest.MustParseNode(`true`)}
+	sut := yamlpathtest.Bool(true)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`true`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("Bool(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("Bool(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestBoolSequence(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{
+		yamltest.MustParseNode(`[true, false, true]`),
+	}
+	sut := yamlpathtest.BoolSequence(true, false, true)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`[true, false, true]`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("BoolSequence(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("BoolSequence(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestFloat(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{yamltest.MustParseNode(`1.23`)}
+	sut := yamlpathtest.Float(1.23)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`1.23`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("Float(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("Float(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestFloatSequence(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{
+		yamltest.MustParseNode(`[1.23, 4.56, 7.89]`),
+	}
+	sut := yamlpathtest.FloatSequence(1.23, 4.56, 7.89)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`[1.23, 4.56, 7.89]`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("FloatSequence(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("FloatSequence(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestObject(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{yamltest.MustParseNode(`{"key": "value"}`)}
+	sut := yamlpathtest.Object(map[string]any{"key": "value"})
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`{"key": "value"}`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("Object(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("Object(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestObjectSequence(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{
+		yamltest.MustParseNode(`[{"key": "value"}, {"key": "value2"}]`),
+	}
+	sut := yamlpathtest.ObjectSequence(
+		map[string]any{"key": "value"},
+		map[string]any{"key": "value2"},
+	)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`[{"key": "value"}, {"key": "value2"}]`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("ObjectSequence(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("ObjectSequence(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestInt(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{yamltest.MustParseNode(`123`)}
+	sut := yamlpathtest.Int(123)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`123`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("Int(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("Int(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestIntSequence(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{
+		yamltest.MustParseNode(`[1, 2, 3]`),
+	}
+	sut := yamlpathtest.IntSequence(1, 2, 3)
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`[1, 2, 3]`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("IntSequence(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("IntSequence(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestNull(t *testing.T) {
+	// Arrange
+	want := yamlpath.Collection{yamlutil.Null()}
+	sut := yamlpathtest.Null()
+
+	// Act
+	got, err := sut.Match(yamltest.MustParseNode(`null`))
+
+	// Assert
+	if got, want := err, (error)(nil); !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Fatalf("Null(...).Match(...) err = %v, want %v", got, want)
+	}
+	if !cmp.Equal(got, want, cmpopts.EquateEmpty()) {
+		t.Errorf("Null(...).Match(...) = %v, want %v", got, want)
+	}
+}
+
+func TestError(t *testing.T) {
+	// Arrange
+	testErr := errors.New("test error")
+
+	// Act
+	sut := yamlpathtest.Error(testErr)
+	_, err := sut.Match(yamltest.MustParseNode(`{}`))
+
+	if got, want := err, testErr; !cmp.Equal(got, want, cmpopts.EquateErrors()) {
+		t.Errorf("Error(...).Match(...) err = %v, want %v", got, want)
+	}
+}

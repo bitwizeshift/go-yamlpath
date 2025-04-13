e4a70d76a155e2c5934863feaebc2d7ae194829a
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2025-04-13 17:42:53 -0400

    Fix `yamlcmp.Equal` to support nested documents
    The `FlattenDocuments` internal function flattens any number of nested
    documents, but previously it assumed only one document was present.
    This fixes the definition to handle multiple nested documents, in case
    a node is ever presented like this.

--- internal/yamlcmp/equal.go
+++ internal/yamlcmp/equal.go
@@ -30,28 +30,36 @@ func Equal(lhs, rhs *yaml.Node) bool {
 	if lhs == nil || rhs == nil {
 		return false
 	}
-	lhs, rhs = yamlconv.FlattenDocuments(lhs)[0], yamlconv.FlattenDocuments(rhs)[0]
+	lhsRange, rhsRange := yamlconv.FlattenDocuments(lhs), yamlconv.FlattenDocuments(rhs)
+	if len(lhsRange) != len(rhsRange) {
+		return false
+	}
 
-	if lhs.Kind != rhs.Kind {
-		return false
-	}
-	if (lhs.Tag == "!!int" || lhs.Tag == "!!float") && (rhs.Tag == "!!int" || rhs.Tag == "!!float") {
-		left, err1 := decimal.NewFromString(lhs.Value)
-		right, err2 := decimal.NewFromString(rhs.Value)
-		if err1 != nil || err2 != nil {
-			return lhs.Value == rhs.Value
-		}
-		return left.Equal(right) && left.NumDigits() == right.NumDigits()
-	}
-	if lhs.Tag != rhs.Tag || lhs.Value != rhs.Value {
-		return false
-	}
-	if len(lhs.Content) != len(rhs.Content) {
-		return false
-	}
-	for i := range lhs.Content {
-		if !Equal(lhs.Content[i], rhs.Content[i]) {
+	for i := range lhsRange {
+		lhs = lhsRange[i]
+		rhs = rhsRange[i]
+
+		if lhs.Kind != rhs.Kind {
 			return false
+		}
+		if (lhs.Tag == "!!int" || lhs.Tag == "!!float") && (rhs.Tag == "!!int" || rhs.Tag == "!!float") {
+			left, err1 := decimal.NewFromString(lhs.Value)
+			right, err2 := decimal.NewFromString(rhs.Value)
+			if err1 != nil || err2 != nil {
+				return lhs.Value == rhs.Value
+			}
+			return left.Equal(right) && left.NumDigits() == right.NumDigits()
+		}
+		if lhs.Tag != rhs.Tag || lhs.Value != rhs.Value {
+			return false
+		}
+		if len(lhs.Content) != len(rhs.Content) {
+			return false
+		}
+		for i := range lhs.Content {
+			if !Equal(lhs.Content[i], rhs.Content[i]) {
+				return false
+			}
 		}
 	}
 	return true
